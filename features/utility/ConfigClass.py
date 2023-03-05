e0756e557fbdd90e51ea3865cdeee776b94afc34
Pavithra-Thieuvelkumaran <pavithrashine28@gmail.com>
2023-03-06 01:25:15 +0530

    Code Commit

---
+++ features/utility/ConfigClass.py
@@ -0,0 +1,6 @@
+class ConfigClass:
+
+    filePath = "C:\\Users\\SEETHALAKSHMI K B\\Downloads\\chromedriver_win32\\chromedriver.exe"
+
+    url = "https://unacademy.com/"
+    url1="https://unacademy.com/manage-goals"
